b3e0c9bebabd2ec105e9adc472ecab976fc56a3b
Clarice Costello <claricostello@outlook.com>
2019-07-08 13:01:35 -0700

    added get methods for ShopController

--- HotSauceApp/HotSauceApp/Controllers/ShopController.cs
+++ HotSauceApp/HotSauceApp/Controllers/ShopController.cs
@@ -12,5 +12,10 @@ namespace HotSauceApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
     }
 }
