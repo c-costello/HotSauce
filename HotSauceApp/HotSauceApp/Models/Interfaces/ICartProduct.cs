94fb6eb02a1e62e3b16f8c82d5885402f91c5cc6
Clarice Costello <claricostello@outlook.com>
2019-07-08 10:54:20 -0700

    Scaffolded file structure for interfaces and services

---
+++ HotSauceApp/HotSauceApp/Models/Interfaces/ICartProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotSauceApp.Models.Interfaces
+{
+    public class ICartProduct
+    {
+    }
+}
