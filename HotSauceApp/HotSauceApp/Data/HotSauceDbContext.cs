3ce4d386359b762a0579f168e47acc37d3f44018
Clarice Costello <claricostello@outlook.com>
2019-07-08 13:34:09 -0700

    Created HotSauceDbContext constructor

--- HotSauceApp/HotSauceApp/Data/HotSauceDbContext.cs
+++ HotSauceApp/HotSauceApp/Data/HotSauceDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using HotSauceApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HotSauceApp.Data
 {
-    public class HotSauceDbContext
+    public class HotSauceDbContext : DbContext
     {
+        public HotSauceDbContext(DbContextOptions<HotSauceDbContext> options): base(options)
+        {
+
+        }
     }
 }
