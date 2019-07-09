922ec2af3e3605c26cefc86b6577ae4e3866e2a9
Clarice Costello <claricostello@outlook.com>
2019-07-09 13:19:54 -0700

    injected user manager and sign in manager to account controller

---
+++ HotSauceApp/HotSauceApp/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotSauceApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotSauceApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _UserManager;
+        private readonly object _SignInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _UserManager = userManager;
+            _SignInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
