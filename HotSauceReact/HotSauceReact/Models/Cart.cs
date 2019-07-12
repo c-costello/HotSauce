54e3ce324caadd0399366d2e3ac8a572d626abe2
Clarice Costello <claricostello@outlook.com>
2019-07-12 13:59:06 -0700

    Created Cart Class

---
+++ HotSauceReact/HotSauceReact/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotSauceReact.Models
+{
+    public class Cart
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+    }
+}
