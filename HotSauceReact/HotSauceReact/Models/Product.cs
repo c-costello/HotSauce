a486e8140beb7f82a44859388442840f6b425893
Clarice Costello <claricostello@outlook.com>
2019-07-12 13:24:29 -0700

    Created abstract product class

---
+++ HotSauceReact/HotSauceReact/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotSauceReact.Models
+{
+    abstract public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public object Size { get; set; }
+        public string SKU { get; set; }
+        public string Description { get; set; }
+
+    }
+}
